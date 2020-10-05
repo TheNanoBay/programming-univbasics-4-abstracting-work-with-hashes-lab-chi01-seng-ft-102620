e3e57a35bd311844282ef552604530f2c30d3412
Mehdi A Noorani <noorani.mehdi@outlook.com>
2020-10-05 14:48:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,12 +7,11 @@ end
 
 def read_from_hash(hash, key)
   value = hash[key]
-  
+  return value 
   # return the correct value using the hash and key parameters
 end
 
 def update_counting_hash(hash, key)
-  
   
   
   # given a hash an a key as parameters, return an updated hash
