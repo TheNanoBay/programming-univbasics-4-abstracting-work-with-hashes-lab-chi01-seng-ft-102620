db6b1d10d4e5674980325c0e23b77b36a283b6d8
Mehdi A Noorani <noorani.mehdi@outlook.com>
2020-10-05 14:47:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  
+  value = hash[key]
   
   # return the correct value using the hash and key parameters
 end
@@ -19,3 +19,4 @@ def update_counting_hash(hash, key)
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
 end
+
